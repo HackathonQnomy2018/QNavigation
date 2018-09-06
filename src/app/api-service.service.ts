ffb7bd52dc13da7de7aac3d8de8d331325baa04b
david.eini <david.eini.se@gmail.com>
2018-09-06 15:37:14 +0300

    sync

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FORMS_BODY } from './central-forms';
+import { EMPTY } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ApiServiceService {
 
-  constructor() { }
+  headers: Headers;
+
+  constructor(private http: HttpClient) {
+
+    this.headers = new Headers()
+    this.headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+    this.headers.append('Access-Control-Allow-Credentials', 'true');
+    this.headers.append('Content-Type', 'application/json;charset=utf-8');
+
+  }
+
+  matrix() {
+    const obj = { Case: '1', Data: null };
+    const body = FORMS_BODY();
+    body.Template.OnlineFormTemplateItems[0].AnswerText = obj;
+    return this.http.post('https://centraldemo.qnomy.com/Forms/form/5A39990E-901E-4B76-820D-BCAFB232342B', body, { headers: headers });
+  }
+
+
+  start(loctionParam) {
+    const obj = { Case: '2', Data: loctionParam.split('=')[1] };
+    const body = FORMS_BODY();
+    body.Template.OnlineFormTemplateItems[0].AnswerText = obj;
+    return this.http.post('https://centraldemo.qnomy.com/Forms/form/5A39990E-901E-4B76-820D-BCAFB232342B', body, { headers: headers });
+  }
+
+
+  customer(personalId) {
+    const obj = { Case: '3', Data: personalId };
+    const body = FORMS_BODY();
+    body.Template.OnlineFormTemplateItems[0].AnswerText = obj;
+    return this.http.post('https://centraldemo.qnomy.com/Forms/form/5A39990E-901E-4B76-820D-BCAFB232342B', body, { headers: headers });
+  }
+
+
+  end(customerId) {
+    const obj = { Case: '4', Data: customerId };
+    const body = FORMS_BODY();
+    body.Template.OnlineFormTemplateItems[0].AnswerText = obj;
+    return this.http.post('https://centraldemo.qnomy.com/Forms/form/5A39990E-901E-4B76-820D-BCAFB232342B', body, { headers: headers });
+  }
 }
